7da72c8b6b5402ce2d9b940fc1e3969bc0407f6a
Heron93 <mawandem98@gmail.com>
2025-12-10 12:00:23 +0200

    Dice game implementation

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images
 var image1 = document.querySelectorAll("img")[0]; 
 
 image1.setAttribute("src",randomImageSource)
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber2 + ".png" //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice2.png
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource)
